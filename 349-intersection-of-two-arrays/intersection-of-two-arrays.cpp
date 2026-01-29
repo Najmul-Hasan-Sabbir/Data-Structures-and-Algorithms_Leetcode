4e18e47c4ae7168981d66dfe737caf4cfa3da217
Najmul-Hasan-Sabbir <156990395+Najmul-Hasan-Sabbir@users.noreply.github.com>
2026-01-29 23:42:59 +0600

    Time: 0 ms (100.00%) | Memory: 13.7 MB (97.29%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -4,24 +4,43 @@ public:
 // this is the brute force approch , im just testing the logic 
 // after that i will use the Two poiters or Hash map solution 
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>result;
-        for(int i=0; i<nums1.size();i++){
+      
+ // Step 1: Sort both arrays
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+vector<int> result;
 
-        for(int j=0; j<nums2.size(); j++){
+        int i = 0; // pointer for nums1
+        int j = 0; // pointer for num
 
-         if(nums1[i]==nums2[j]){
+        while(i<nums1.size()&& j<nums2.size()){
 
-            if(find(result.begin(), result.end(), nums1[i]) == result.end()){
-                result.push_back(nums1[i]);
-            }
-         }
+      
+      if(nums1[i]==nums2[j]){
+        // To ensure uniqueness in result
+if(result.empty()|| result.back()!=nums1[i]){
 
-        }
+    result.push_back(nums1[i]);
+}
+
+
+
+        i++;
+        j++;
+      }
+       // If nums1 element is smaller, move i
+      else if(nums1[i]<nums2[j]){
+        i++;
+      }
+        // If nums2 element is smaller, move j
+      else{
+        j++;
+      }
+
 
         }
 
 
 return result;
-
     }
 };
